e5b9d47b2793de57bdec348ac74983f7c09bab2d
Yuheng <miaoyh1997k@gmail.com>
2021-12-30 17:28:17 -0800

    set up routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 
 const app = express();
 
@@ -10,3 +9,28 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+/* Set up home page */
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+/* Set up login page */
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+/* Set up register page */
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+/* Set up server */
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
